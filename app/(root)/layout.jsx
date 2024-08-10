8fb91f4908426b8e6b143b6fed6d33f4ab80a4f5
dev-kristian <kristian.aleksi@hotmail.com>
2024-08-10 12:00:22 +0200

    fix mobile layout

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -10,20 +10,20 @@ import Image from "next/image";
 function RootLayout({ children }) {
   return (
     <main className="flex h-screen w-full font-inter">
-      <Sidebar/>
-      <div className="flex size-full flex-col overflow-y-hidden ">
-        <div className="root-layout">
+      <Sidebar />
+      <div className="flex flex-col w-full h-full overflow-hidden">
+        <div className="root-layout md:hidden">
           <Image
             src='/icons/logo.png'
             width={30}
             height={30}
             alt='logo'
           />
-          <div>
-            <MobileNav/>
-          </div>
+          <MobileNav />
         </div>
-        {children}
+        <div className="flex-grow overflow-auto">
+          {children}
+        </div>
       </div>
     </main>
   );
