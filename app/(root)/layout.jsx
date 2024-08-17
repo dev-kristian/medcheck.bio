dde8e26535774707555de5cc6ddbbdb381ff67fb
dev-kristian <kristian.aleksi@hotmail.com>
2024-08-17 23:15:33 +0200

    slight layout fix

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -17,20 +17,7 @@ function RootLayout({ children }) {
           <ConversationsProvider>
           <main className="flex h-screen w-full font-inter">
             <Sidebar user={user}/>
-            <div className="flex size-full flex-col">
-              <div className="root-layout ">
-                <Image
-                  src='/icons/logo.png'
-                  width={30}
-                  height={30}
-                  alt='logo'
-                />
-                <div>
-                  <MobileNav user={user}/>
-                </div>
-              </div>
               {children}
-            </div>
           </main>
           </ConversationsProvider>
         </TestProvider>
