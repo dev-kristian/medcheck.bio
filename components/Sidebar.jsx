e7888793d7562121a12a544606021d37a69a1beb
dev-kristian <kristian.aleksi@hotmail.com>
2024-08-17 19:25:02 +0200

    slight layout fix

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -23,9 +23,9 @@ const Sidebar = ({user}) => {
   };
 
   return (
-    <section className='sidebar flex flex-col justify-between'>
+    <section className='sidebar flex flex-col justify-between '>
       <div>
-        <nav className='flex flex-col gap-4 px-2'>
+        <nav className='flex flex-col gap-4 px-2 h-[calc(100vh-90px)] overflow-y-auto'>
           <Link href="/" className='mb-8 cursor-pointer flex items-center gap-2 mr-4'>
               <Image
                   src='/icons/logo.png'
